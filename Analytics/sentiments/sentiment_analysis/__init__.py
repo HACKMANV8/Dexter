640dc601cb92fb3e2e14f455f2ddd3d95b609efe
Ashutosh <ashutoshbhatt2609@gmail.com>
2025-10-31 22:54:05 +0530

    chore: Remove outdated sentiment analysis data files and update package initialization

--- Analytics/sentiments/sentiment_analysis/__init__.py
+++ Analytics/sentiments/sentiment_analysis/__init__.py
@@ -1,4 +1,13 @@
+"""
+This file makes the 'sentiment_analysis' directory a Python package
+and exposes its primary functions.
+"""
+
 from .analyzer import analyze_company
 from .model import load_sentiment_pipeline
 
-__all__ = ["analyze_company", "load_sentiment_pipeline"]
+__all__ = [
+    "analyze_company",
+    "load_sentiment_pipeline"
+]
+
